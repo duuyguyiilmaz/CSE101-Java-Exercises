7e182fdc9945c21c0ac36dce668d36f3a49ad741
Duygu <duygu.147@hotmail.com>
2025-06-24 18:22:17 +0300

    Create DiceProbability.java
    Add probability calculation for first roll of 4 on kth trial

---
+++ DiceProbability.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class DiceProbability {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your number k trial:");
+        int k = scn.nextInt();
+        double DiceProbibility = Math.pow(((5.0 / 6)), (k - 1)) * (1.0 / 6);
+        System.out.printf("The probability of rolling the first 4 on trial <%d> is: %.6f\n", k, diceProbability);
+
+    }
+}
